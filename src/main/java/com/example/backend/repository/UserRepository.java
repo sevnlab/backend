82c05e572bcff859d8ebb9ee4fc95513a620ef69
seven <xodnr1531@gmail.com>
2026-03-19 00:05:40 +0900

    프로젝트명 변경

--- src/main/java/com/example/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Member, Long> {
-    Member findByMemberId(String memberId);  // userId로 사용자 조회
-}
